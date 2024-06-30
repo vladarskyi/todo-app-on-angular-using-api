c9dc147f22a326a18ce32c0e9b2bacf5225d6dad
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-06-30 20:35:26 +0300

    move todo app from react

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { TodosPageComponent } from './components/todos-page/todos-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    TodosPageComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'todo-app-on-angular-using-api';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TodosPageComponent } from './components/todos-page/todos-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutPageComponent },
+  { path: 'todos/:status', component: TodosPageComponent },
+  { path: '**', redirectTo: 'todos/all', pathMatch: 'full' },
+];
